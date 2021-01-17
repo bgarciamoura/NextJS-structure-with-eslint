db98dc26e5dc3e7ddec6cef9a123aebe65a6eb7a
Bruno Garcia <bgarciamoura@gmail.com>
2021-01-17 15:23:44 -0300

    Project structure for NextJS apps with eslint configured

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line prettier/prettier
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from '../styles/global';
+import theme from '../styles/theme';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+	<ThemeProvider theme={theme}>
+		<Component {...pageProps} />
+		<GlobalStyle />
+	</ThemeProvider>
+);
+
+export default MyApp;
